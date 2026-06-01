f0a6943804f4cd57c17a64226e9cbb290ae1de16
Roberto Delgado <216353140+delgadoroberto@users.noreply.github.com>
2026-05-31 23:09:53 -0300

    Fix overly permissive RDS security group egress rule

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -144,11 +144,11 @@ resource "aws_security_group_rule" "ingress" {
 
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.default.id}"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.web_vpc.cidr_block]
+  security_group_id = aws_security_group.default.id
 }
 
 
